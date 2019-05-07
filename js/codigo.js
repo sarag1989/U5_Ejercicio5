71a9624e10dd0d3e09b7fca76eaf66f675b39973
Sara Gharafi Abalkais <saraadr2019@gmail.com>
2019-05-07 13:59:23 +0200

    arreglos finales

--- js/codigo.js
+++ js/codigo.js
@@ -5,15 +5,15 @@ en varias
 lineas */
 
 let miNombre = prompt("Introduzca su nombre");
-/* if (miNombre === "Pepito Grillo"){
+if (miNombre === "Pepito Grillo"){
   alert("saludos, SR. Grillo");
 }else {
   alert("Usted no es el usuario deseado");
-} */
-if (miNombre === "Pepito Grillo"){
+}
+/* if (miNombre === "Pepito Grillo"){
   alert("saludos, SR. Grillo");
 }
 while (miNombre !== "Pepito Grillo") {
   alert("Usted no es el usuario deseado");
   miNombre = prompt("Vuelva a introducir su nombre");
-}
+} */
